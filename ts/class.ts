433315feeded4881f24fd0418fa02a2170435a15
siotan <nyaa.-525@outlook.jp>
2022-12-18 15:03:27 +0900

    class

---
+++ ts/class.ts
@@ -0,0 +1,10 @@
+class Person {
+  name: string;
+  constructor(initName: string) {
+    this.name = initName;
+  }
+  greeting() {
+    console.log(`Hello! My name is ${this.name}`);
+  }
+}
+const quill = new Person("quill");
